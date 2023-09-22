5339accad2eee9c765d3cfda1dd462164f2426c7
shubham <shubham.giri@thinkscoopinc.com>
2023-09-22 11:36:10 +0530

    analytics

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga';
 
 export const initGA = () => {
   console.log('GA init');
-  ReactGA.initialize('UA-xxxxxxxxx-1');
+  ReactGA.initialize('G-6T6QKWM7RS');
 };
 
 export const logPageView = () => {
