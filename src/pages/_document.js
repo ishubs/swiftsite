c94aba727c010ca9e8618930d2b066c296f2f3ff
shubham giri <41017859+ishubs@users.noreply.github.com>
2024-03-23 22:02:34 +0530

    script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import { Script } from 'next';
 class CustomDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
